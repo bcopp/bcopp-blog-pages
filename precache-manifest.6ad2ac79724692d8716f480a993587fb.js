0eb2e3ab7a667ea2ffd7fd61c261b401ccd74cbe
bcopp <bcopp.oss@gmail.com>
2020-05-28 20:28:07 -0700

    Updates

---
+++ precache-manifest.6ad2ac79724692d8716f480a993587fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7909c7fda7a91b01a1f4db97e3918d34",
+    "url": "/bcopp-blog-pages/index.html"
+  },
+  {
+    "revision": "cd5985f6810b0f5ac485",
+    "url": "/bcopp-blog-pages/static/css/2.8b358cb9.chunk.css"
+  },
+  {
+    "revision": "86a247c28468f2f22183",
+    "url": "/bcopp-blog-pages/static/css/main.702b203b.chunk.css"
+  },
+  {
+    "revision": "cd5985f6810b0f5ac485",
+    "url": "/bcopp-blog-pages/static/js/2.cea9690a.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/bcopp-blog-pages/static/js/2.cea9690a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a247c28468f2f22183",
+    "url": "/bcopp-blog-pages/static/js/main.2b4420e6.chunk.js"
+  },
+  {
+    "revision": "6b398ca655e5cce8dc0d",
+    "url": "/bcopp-blog-pages/static/js/runtime-main.62e64461.js"
+  },
+  {
+    "revision": "c8ffdeb3144d5055756ef01ef98e8486",
+    "url": "/bcopp-blog-pages/static/media/OpenSans-Regular-webfont.c8ffdeb3.woff"
+  },
+  {
+    "revision": "d58ce2fb40c2356e4c43eb0f93b3c122",
+    "url": "/bcopp-blog-pages/static/media/headshot.d58ce2fb.jpg"
+  }
+]);
